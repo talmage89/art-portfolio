4c8b69001176f9c864fbf5f01584a02d495d2194
talmage89 <talmage.bergeson@gmail.com>
2024-11-13 16:22:19 -0700

    Ensure test views are only accessible in API when DEBUG is true

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -1,25 +1,9 @@
-"""
-URL configuration for portfolio project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
+
 from artwork.views import (
     ArtworkViewSet,
     ImageViewSet,
@@ -42,18 +26,12 @@ urlpatterns = [
         name="create-checkout-session",
     ),
     path("api/stripe-webhook/", stripe_webhook, name="stripe-webhook"),
-    path(
-        "api/test-send-email/",
-        TestEmailSendView.as_view(),
-        name="test-send-email",
-    ),
-    path(
-        "api/preview-email/",
-        PreviewEmailTemplateView.as_view(),
-        name="preview-email",
-    ),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns.extend([
+        path("api/preview-email/", PreviewEmailTemplateView.as_view(), name="preview-email"),
+        path("api/test-send-email/", TestEmailSendView.as_view(), name="test-send-email"),
+    ])
