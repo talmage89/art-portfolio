242be397b189d41312a28878282cd5f93061d4ec
talmage89 <talmage.bergeson@gmail.com>
2024-11-18 13:22:43 -0700

    Update development settings.py

--- backend/portfolio/settings/development.py
+++ backend/portfolio/settings/development.py
@@ -1,8 +1,12 @@
 from .base import *
 
+SERVER_IP = os.getenv("SERVER_IP")
+PRODUCTION_DOMAIN = os.getenv("PRODUCTION_DOMAIN")
+
+
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", SERVER_IP, PRODUCTION_DOMAIN]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
