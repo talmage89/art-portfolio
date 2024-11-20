6be4e79a6b833d00100fd5eb4c2bf0c810a0e3ef
talmage89 <talmage.bergeson@gmail.com>
2024-11-20 13:42:59 -0700

    Configure production settings to require SSL

--- backend/portfolio/settings/production.py
+++ backend/portfolio/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 
 DOMAIN = env("PRODUCTION_DOMAIN")
 FRONTEND_URL = DOMAIN
-BASE_URL = f"http://{DOMAIN}"
+BASE_URL = f"https://{DOMAIN}"
 
 DEBUG = False
 
@@ -13,21 +13,19 @@ STATIC_ROOT = env("STATIC_ROOT")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = env("MEDIA_ROOT")
 
-# SESSION_COOKIE_DOMAIN = DOMAIN
-SESSION_COOKIE_DOMAIN = None
+SESSION_COOKIE_DOMAIN = DOMAIN
 SESSION_COOKIE_AGE = 1209600
 SESSION_COOKIE_HTTPONLY = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-# SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [f"https://{DOMAIN}", f"https://www.{DOMAIN}"]
-# CSRF_COOKIE_DOMAIN = DOMAIN
-CSRF_COOKIE_DOMAIN = None
+CSRF_COOKIE_DOMAIN = DOMAIN
 CSRF_TRUSTED_ORIGINS = [f"https://{DOMAIN}", f"https://www.{DOMAIN}"]
-# CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 # SECURE_HSTS_SECONDS = 60 * 60 * 24 * 365
 # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 # SECURE_HSTS_PRELOAD = True
@@ -37,9 +35,35 @@ X_FRAME_OPTIONS = "DENY"
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_REFERRER_POLICY = "same-origin"
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
 ]
+
+CSP_DEFAULT_SRC = ("'none'",)
+CSP_CONNECT_SRC = (
+    "'self'",
+    "https://api.stripe.com",
+)
+CSP_SCRIPT_SRC = (
+    "'self'",
+    "'unsafe-inline'",
+    "'unsafe-eval'",
+)
+CSP_STYLE_SRC = (
+    "'self'",
+    "'unsafe-inline'",
+)
+CSP_IMG_SRC = (
+    "'self'",
+    "data:",
+)
+CSP_FONT_SRC = ("'self'",)
+CSP_FRAME_SRC = ("'none'",)
+CSP_FRAME_ANCESTORS = ("'none'",)
+CSP_OBJECT_SRC = ("'none'",)
+CSP_MEDIA_SRC = ("'none'",)
+CSP_FORM_ACTION = ("'self'",)
+CSP_WORKER_SRC = ("'none'",)
