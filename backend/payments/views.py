bb16c060f1c3e0155c63d74529ebc6820a8b7432
talmage89 <talmage.bergeson@gmail.com>
2024-11-20 14:05:22 -0700

    Configure Stripe Session payment method types

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -82,6 +82,11 @@ class CreateCheckoutSessionView(views.APIView):
                     success_url=f"{settings.FRONTEND_URL}/checkout/success",
                     cancel_url=f"{settings.FRONTEND_URL}/",
                     expires_at=int(time.time() + 60 * 30),
+                    payment_method_types=[
+                        "card",
+                        "apple_pay",
+                        "google_pay",
+                    ],
                     metadata={
                         "product_ids": product_ids_str,
                         "created_at": str(timezone.now()),
